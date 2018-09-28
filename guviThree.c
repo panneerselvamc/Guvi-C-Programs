df147aac82beab212ac286c943d97a08b9e1d5b8
panneer selvam.c <31854641+panneerselvamc@users.noreply.github.com>
2018-09-28 18:58:02 +0530

    Create guviThree.c

---
+++ guviThree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+    printf("vowel");
+}
+else
+printf("consonents");
+getch();
+}
