9222d1da5c89d1808a40acddb61956df65df86e3
panneer selvam.c <31854641+panneerselvamc@users.noreply.github.com>
2018-09-28 18:50:44 +0530

    Create guviTen.c

---
+++ guviTen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int low, high, i, f;
+    scanf("%d %d", &low, &high);
+    while (low < high)
+    {
+        f=0;
+        for(i = 2; i <= low/2; ++i)
+        {
+        if(low % i == 0)
+            {
+                f=1;
+                break;
+            }
+        }
+
+        if (f== 0)
+            printf("%d ", low);
+            ++low;
+    }
+    return 0;
+}
